d7309411bc6966d06a75e2b5b0470f049bec85c8
anoopraju31 <anoopraju31@gmail.com>
2023-10-05 20:59:13 +0530

    Server - Auth route - test user added

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,11 +1,26 @@
 import express from 'express'
+import User from '../models/User.model'
 
-export const signUpController = (
+export const signUpController = async (
 	req: express.Request,
 	res: express.Response,
 ) => {
-	console.log(req.body)
+	try {
+		const { username, email, password } = req.body
 
-	res.status(200)
-	res.json({ message: 'ok' })
+		if (!username || !email || !password) {
+			return res.status(400).json({
+				message: '',
+			})
+		}
+
+		const newUser = new User({ username, email, password })
+
+		await newUser.save()
+
+		res.status(201).json({ message: 'User created successfully!' })
+	} catch (error) {
+		console.error(error)
+		return res.sendStatus(400).json({ message: error.message })
+	}
 }
