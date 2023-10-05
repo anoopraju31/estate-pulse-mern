c1e0f3d56dd876ee43a307a24600e36987b4fcdb
anoopraju31 <anoopraju31@gmail.com>
2023-10-05 16:43:07 +0530

    Client - Pages - Sign Page - initial

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as About } from './About'
 export { default as SignIn } from './SignIn'
-export { default as SignOut } from './SignOut'
+export { default as SignUp } from './SignUp'
 export { default as Profile } from './Profile'
 export { default as Home } from './Home'
