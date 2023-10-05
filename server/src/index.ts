4bc1e7d12e88ada6eafdea7593d670a605737310
anoopraju31 <anoopraju31@gmail.com>
2023-10-05 19:26:09 +0530

    Server - MongoDB Connection added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// * MongoDB Connection
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => {
+		console.log('Connected to MongoDB')
+	})
+	.catch((error) => {
+		console.error(error)
+	})
 
 const app = express()
 const PORT = 8080
