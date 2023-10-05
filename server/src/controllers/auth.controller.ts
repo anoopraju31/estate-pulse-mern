700de43667a50dd71a3a87ce456ee9b03a2083cf
anoopraju31 <anoopraju31@gmail.com>
2023-10-05 21:43:04 +0530

    Server - Auth route -sign route completed

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,12 @@
 import express from 'express'
+import bcryptjs from 'bcryptjs'
 import User from '../models/User.model'
+
+// * regex patterns
+const usernameRegex = /^[A-Za-z]+( [A-Za-z]+)?$/
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const passwordRegex =
+	/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 
 export const signUpController = async (
 	req: express.Request,
@@ -8,19 +15,53 @@ export const signUpController = async (
 	try {
 		const { username, email, password } = req.body
 
+		// * validation - check for all fields are present or not
 		if (!username || !email || !password) {
-			return res.status(400).json({
-				message: '',
+			res.status(400).json({
+				error: 'please fill all fields',
 			})
+
+			return
 		}
 
-		const newUser = new User({ username, email, password })
+		// * validation - check for valid username
+		if (!usernameRegex.test(username)) {
+			res.status(400).json({
+				error:
+					'Invalid username. Usernames can only contain alphabetic characters (A-Z, a-z).',
+			})
+
+			return
+		}
+
+		// * validation - check for invaild email
+		if (!emailPattern.test(email)) {
+			res.status(400).json({
+				error: 'Invalid email address. Please enter a valid email.',
+			})
+
+			return
+		}
+
+		// * validation - check for password
+		if (!passwordRegex.test(password)) {
+			res.status(400).json({
+				error:
+					'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.',
+			})
+
+			return
+		}
+
+		// ? hashing password
+		const hashedPassword = await bcryptjs.hash(password, 12)
+		const newUser = new User({ username, email, password: hashedPassword })
 
 		await newUser.save()
 
-		res.status(201).json({ message: 'User created successfully!' })
+		return res.status(201).json({ message: 'User created successfully!' })
 	} catch (error) {
-		console.error(error)
-		return res.sendStatus(400).json({ message: error.message })
+		//! console.error(error)
+		return res.status(500).json({ message: error.message })
 	}
 }
