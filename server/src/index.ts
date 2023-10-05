11b1f24414c37a832dd4d7ad354e81c24fe0b394
anoopraju31 <anoopraju31@gmail.com>
2023-10-05 19:58:00 +0530

    Server - User Router - initial

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route'
+
 dotenv.config()
 
 // * MongoDB Connection
@@ -15,6 +17,8 @@ mongoose
 
 const app = express()
 const PORT = 8080
+
+app.use('/api/user', userRouter)
 
 app.listen(PORT, () => {
 	console.log(`Server started at port ${PORT}.`)
