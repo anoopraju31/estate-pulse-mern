c6363b49b21aafe1e82af98e5a5b9b78bde05fb1
anoopraju31 <anoopraju31@gmail.com>
2023-10-05 16:37:10 +0530

    Client - Pages: Profile Page initial

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as About } from './About'
 export { default as SignIn } from './SignIn'
 export { default as SignOut } from './SignOut'
+export { default as Profile } from './Profile'
