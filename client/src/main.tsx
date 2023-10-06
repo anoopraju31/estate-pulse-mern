4bd9e1b717752c5f4b46ecd01cb293e0e8f880f3
anoopraju31 <anoopraju31@gmail.com>
2023-10-07 01:22:19 +0530

    Client - Add Redux Persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
-import { store } from './app/store.ts'
+import { persistor, store } from './app/store.ts'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 )
