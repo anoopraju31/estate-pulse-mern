fa686b2e99012dc877d79b47de9cb98dbe1737da
anoopraju31 <anoopraju31@gmail.com>
2023-10-07 21:44:07 +0530

    Client - Profile Page Private

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,7 +1,9 @@
-interface PrivateRouteProps {}
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAppSelector } from '../app/hooks'
 
-const PrivateRoute = (props: PrivateRouteProps) => {
-	return <div>PrivateRoute</div>
+const PrivateRoute = () => {
+	const { currentUser } = useAppSelector((state) => state.user)
+	return currentUser ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
 export default PrivateRoute
