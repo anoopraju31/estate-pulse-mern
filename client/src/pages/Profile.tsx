3e1ba6c67d756c63a28abed0c7a9b3a424576836
anoopraju31 <anoopraju31@gmail.com>
2023-10-09 02:38:57 +0530

    Client - Profile Page - Delete Account Button styled

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -7,11 +7,52 @@ import {
 	signOutUserSuccess,
 } from '../reducers/userSlice'
 import toast from 'react-hot-toast'
+import { useEffect, useState } from 'react'
+
+interface ModelProps {
+	cancel: () => void
+	proceed: () => void
+}
+
+const Model = ({ cancel, proceed }: ModelProps) => (
+	<div className=' fixed top-0 left-0 right-0 h-screen z-50 flex justify-center items-center backdrop-blur-md bg-gray-900/10 dark:bg-gray-100/10'>
+		<div className='p-10 max-w-lg bg-gray-100 dark:bg-gray-900 rounded-xl'>
+			<h1 className='text-red-500 font-bold text-center text-xl'>
+				{' '}
+				Are you sure? <br className='md:hidden' /> you want to delete your
+				account?{' '}
+			</h1>
+
+			<div className='mt-10 flex justify-end gap-4'>
+				{/* Delete account */}
+				<button
+					onClick={proceed}
+					className='w-full sm:w-fit py-2 px-4 bg-red-600 text-white rounded-lg font-medium'
+					type='button'>
+					Delete
+				</button>
+
+				{/* Cancel */}
+				<button
+					onClick={cancel}
+					className='w-full sm:w-fit py-2 px-4 bg-gray-600 dark:bg-white text-white dark:text-gray-900 rounded-lg font-medium'
+					type='button'>
+					Cancel
+				</button>
+			</div>
+		</div>
+	</div>
+)
 
 const ProfilePage = () => {
+	const [isModelOpen, setIsModelOpen] = useState(false)
 	const { currentUser } = useAppSelector((state) => state.user)
 	const auth = getAuth()
 	const dispatch = useAppDispatch()
+
+	useEffect(() => {
+		document.body.style.overflow = isModelOpen ? 'hidden' : 'visible'
+	}, [isModelOpen])
 
 	const handleSignOut = () => {
 		dispatch(signOutUserStart())
@@ -25,6 +66,8 @@ const ProfilePage = () => {
 				toast.error(error.message)
 			})
 	}
+
+	const handleDeleteAccount = () => {}
 
 	return (
 		<main className='bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white'>
@@ -58,7 +101,7 @@ const ProfilePage = () => {
 						{/* Edit */}
 						<Link
 							to='/edit-profile'
-							className='w-full sm:w-fit py-2 px-4 bg-gray-600 dark:bg-white text-white dark:text-gray-900 rounded-lg font-medium'>
+							className='w-full sm:w-fit py-2 px-4 bg-gray-600 dark:bg-white text-white dark:text-gray-900 text-center rounded-lg font-medium'>
 							{' '}
 							Edit profile{' '}
 						</Link>
@@ -66,7 +109,7 @@ const ProfilePage = () => {
 						{/* Create Listing */}
 						<Link
 							to='/create-listing'
-							className='w-full sm:w-fit py-2 px-4 bg-gray-600 dark:bg-white text-white dark:text-gray-900 rounded-lg font-medium'
+							className='w-full sm:w-fit py-2 px-4 bg-gray-600 dark:bg-white text-white dark:text-gray-900 text-center rounded-lg font-medium'
 							type='button'>
 							{' '}
 							Create listing{' '}
@@ -78,6 +121,14 @@ const ProfilePage = () => {
 							className='w-full sm:w-fit py-2 px-4 bg-gray-600 dark:bg-white text-white dark:text-gray-900 rounded-lg font-medium'
 							type='button'>
 							SignOut
+						</button>
+
+						{/* Delete account */}
+						<button
+							onClick={() => setIsModelOpen(true)}
+							className='w-full sm:w-fit py-2 px-4 bg-red-600 text-white rounded-lg font-medium'
+							type='button'>
+							Delete
 						</button>
 					</div>
 				</div>
@@ -91,6 +142,13 @@ const ProfilePage = () => {
 					<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6'></div>
 				</div>
 			</div>
+
+			{isModelOpen && (
+				<Model
+					proceed={handleDeleteAccount}
+					cancel={() => setIsModelOpen(false)}
+				/>
+			)}
 		</main>
 	)
 }
