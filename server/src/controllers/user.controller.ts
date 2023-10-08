3128a732267f1e2fa6c53b6f81fbfb0bf49cd2b7
anoopraju31 <anoopraju31@gmail.com>
2023-10-09 02:42:59 +0530

    Server - Delete Account Route - added

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -57,7 +57,15 @@ export const deleteUser = async (
 	res: Response,
 	next: NextFunction,
 ) => {
-	res.json({
-		message: 'Api route is working',
-	})
+	if (req.user.id !== req.params.id)
+		return next(errorHandler(401, 'You can only update your own account'))
+
+	try {
+		await User.findByIdAndDelete(req.params.id)
+		res.clearCookie('access_token')
+		res.status(200).json('User has been deleted!')
+	} catch (error) {
+		console.log(error)
+		next(error)
+	}
 }
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { test, updateUser } from '../controllers/user.controller'
+import { deleteUser, test, updateUser } from '../controllers/user.controller'
 import { verifyToken } from '../utills/verifyUser'
 
 const router = express.Router()
 
 router.get('/test', test)
 router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
 
 export default router
