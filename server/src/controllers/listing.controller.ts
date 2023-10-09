e2d2c1dcbd750165d803f66bb76c0190425a53fd
anoopraju31 <anoopraju31@gmail.com>
2023-10-09 13:47:54 +0530

    Server - Listing Route - working

--- server/src/controllers/listing.controller.ts
+++ server/src/controllers/listing.controller.ts
@@ -4,4 +4,12 @@ export const createListing = async (
 	req: Request,
 	res: Response,
 	next: NextFunction,
-) => {}
+) => {
+	try {
+		res.json({
+			message: 'Api route is working',
+		})
+	} catch (error) {
+		next(error)
+	}
+}
