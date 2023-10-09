f98d2496260795ba2c494899fe50ebdc678d9797
anoopraju31 <anoopraju31@gmail.com>
2023-10-09 13:18:34 +0530

    Server - Routes export change

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import userRouter from './routes/user.route'
-import authRouter from './routes/auth.route'
 import { errorHandler } from './middlewares/errorHandler.middleware'
+import { authRouter, listingRoute, userRouter } from './routes'
 
 dotenv.config()
 
@@ -24,8 +23,9 @@ const PORT = 8080
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+app.use('/api/listind', listingRoute)
 
 app.use(errorHandler)
 
---
+++ server/src/routes/index.ts
@@ -0,0 +1,3 @@
+export { default as authRouter } from './auth.route'
+export { default as userRouter } from './user.route'
+export { default as listingRoute } from './listing.route'
