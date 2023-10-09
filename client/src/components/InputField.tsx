88ed0c6e09f19ffea7505c4e35f6aad2dd4e5f14
anoopraju31 <anoopraju31@gmail.com>
2023-10-09 21:53:25 +0530

    Client - Input Field Component - Text Area added

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,13 +1,17 @@
 interface InputFieldProps {
+	isTextArea?: boolean
 	label: string
 	name: string
-	type: string
+	type?: string
 	placeholder: string
 	value: string | number
-	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+	handleChange: (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+	) => void
 }
 
 const InputField = ({
+	isTextArea,
 	label,
 	name,
 	type,
@@ -21,15 +25,28 @@ const InputField = ({
 				{label}
 			</label>
 
-			<input
-				type={type}
-				id={name}
-				name={name}
-				placeholder={placeholder}
-				value={value}
-				onChange={handleChange}
-				className='w-full p-3 pe-12 bg-transparent border border-gray-300 dark:border-gray-500  focus:border-green-500 rounded-lg text-gray-900 dark:text-white text-base shadow-sm outline-none'
-			/>
+			{isTextArea ? (
+				<textarea
+					id={name}
+					name={name}
+					placeholder={placeholder}
+					value={value}
+					onChange={handleChange}
+					className='w-full p-3 pe-12 bg-transparent border border-gray-300 dark:border-gray-500  focus:border-green-500 rounded-lg text-gray-900 dark:text-white text-base shadow-sm outline-none'
+					cols={20}
+					rows={10}></textarea>
+			) : (
+				<input
+					type={type}
+					id={name}
+					name={name}
+					step='1'
+					placeholder={placeholder}
+					value={value}
+					onChange={handleChange}
+					className='w-full p-3 pe-12 bg-transparent border border-gray-300 dark:border-gray-500  focus:border-green-500 rounded-lg text-gray-900 dark:text-white text-base shadow-sm outline-none'
+				/>
+			)}
 		</div>
 	)
 }
