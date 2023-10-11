1eee387b183daa0be72f100ff3841464675e080e
anoopraju31 <anoopraju31@gmail.com>
2023-10-11 14:43:28 +0530

    Client - Home Page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 const HomePage = () => {
-	return <div>HomePage</div>
+	return (
+		<main className='bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white'>
+			<div className='max-w-screen-xl min-h-[var(--container-min-height)] mx-auto py-4 md:py-8 px-4 sm:px-6 lg:px-8 flex flex-col'></div>
+		</main>
+	)
 }
 
 export default HomePage
